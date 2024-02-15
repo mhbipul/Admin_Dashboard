222ecdbb8318f33cf889bb8f0ee92dee9b5f8094
Bipul <mhbipul99bd@gmail.com>
2024-02-15 23:26:58 +0600

    Header & Sidebar completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
+import Home from "./components/Home"
+
+function App() {
+ 
+
+  return (
+    <div className='grid-container'>
+
+      <Header />
+      <Sidebar />
+      <Home />
+
+      
+    
+      
+      
+    </div>
+  )
+}
+
+export default App
